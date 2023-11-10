2034597963bcf08af673af3600f4fa73c059110f
Melissa Canu <melissacanu89@gmail.com>
2023-11-10 11:38:40 +0100

    trovato error, functionex3 invocata due volte

--- D10.js
+++ D10.js
@@ -103,6 +103,14 @@ console.log(whoIsBigger2(30, 13));
   Es.: splitMe("I love coding") => ritorna ["I", "Love", "Coding"]
 */
 
+function splitMe(string) {
+  return string.split(" ");
+}
+let string = "Ci voglio credere";
+let arrSplitString = splitMe(string); // è qua che invoco la funzione?
+
+console.log(arrSplitString);
+
 /* ESERCIZIO 4
   Crea una funzione chiamata "deleteOne" che riceve una stringa e un booleano come parametri.
   Se il valore booleano è true la funzione deve ritornare la stringa senza il primo carattere, altrimenti la deve ritornare senza l'ultimo.
